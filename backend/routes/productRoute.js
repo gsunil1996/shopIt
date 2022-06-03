c76dbc27d3fc3ff3eff425e4c7c642a398c4a0c0
gsunil1996 <gajulasunilkumar123@gmail.com>
2022-06-04 01:59:57 +0530

    error handling added

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,12 +2,19 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getProducts, newProduct } = require("../controllers/productController");
+const {
+  getProducts,
+  newProduct,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productController");
 
-// router.route("/products").get(getProducts);
 router.get("/products", getProducts);
+router.get("/product/:id", getSingleProduct);
 
-// router.route("/product/new") / this.post(newProduct);
-router.post("/product/new", newProduct);
+router.post("/admin/product/new", newProduct);
+router.put("/admin/product/:id", updateProduct);
+router.delete("/admin/product/:id", deleteProduct);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,30 @@ const connectDatabase = require("./config/database");
 
 const dotenv = require("dotenv");
 
+// Handle the uncaught exceptions
+process.on("uncaughtException", err => {
+    console.error(`ERROR: ${err.stack}`);
+    console.log("Shutting down due to uncaught exception");
+    process.exit(1);
+})
+
 // Setting up config file
 dotenv.config({ path: "backend/config/config.env" });
 
 // Connecting to database
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
-})
+const server = app.listen(process.env.PORT, () => {
+  console.log(
+    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
+});
+
+// Handle Unhandled Promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err.message}`),
+    console.error("Shutting down the server due to unhandled rejection");
+  server.close(() => {
+    process.exit(1);
+  });
+});
