89447161cd102ca31a05494656cbd873ca27adb2
gsunil1996 <gajulasunilkumar123@gmail.com>
2022-06-05 22:55:53 +0530

    user routes added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -135,6 +135,51 @@ exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
 })
 
 
+// update / Change Password => /api/v1/password/update
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+
+  const user = await User.findById(req.user.id).select("+password");
+
+  // Check previous user password
+  const isMatched = await user.comparePassword(req.body.oldPassword);
+
+  if (!isMatched) {
+    return next(new ErrorHandler("old password is incorrect", 400));
+  }
+
+  user.password = req.body.password;
+
+  await user.save();
+
+  sendToken(user, 200, res);
+
+})
+
+
+// update user profile => api/v1/me/update
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+
+  }
+
+  // Update avatar: TODO
+
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: true
+  })
+
+  res.status(200).json({
+    success: true,
+  })
+
+})
+
+
 // Logout user => /api/v1/Logout
 exports.logout = catchAsyncErrors(async (req, res, next) => {
   res.cookie("token", null, {
@@ -148,3 +193,79 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
   })
 
 });
+
+
+
+// Admin routes
+
+// Get all users => /api/v1/admin/users
+exports.allUsers = catchAsyncErrors(async (req, res, next) => {
+
+  const users = await User.find();
+
+  res.status(200).json({
+    success: true,
+    users
+  })
+})
+
+
+// Get user details => /api/v1/admin/user/:id
+exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
+
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(new ErrorHandler(`User does not found with id: ${req.params.id}`));
+  }
+
+  res.status(200).json({
+    success: true,
+    user
+  })
+  
+})
+
+
+// update user => /api/v1/admin/user/:id
+exports.updateUser = catchAsyncErrors(async (req, res, next) => {
+
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role
+
+  }
+
+
+  const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: true
+  })
+
+  res.status(200).json({
+    success: true,
+  })
+
+})
+
+
+// Delete user => /api/v1/admin/user/:id
+exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(new ErrorHandler(`User does not found with id: ${req.params.id}`));
+  }
+
+  // Remove avatar from cloudinary - TODO
+
+  await user.remove();
+
+  res.status(200).json({
+    success: true,
+  })
+  
+})
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -8,9 +8,15 @@ const {
   forgotPassword,
   resetPassword,
   getUserProfile,
+  updatePassword,
+  updateProfile,
+  allUsers,
+  getUserDetails,
+  updateUser,
+  deleteUser,
 } = require("../controllers/authController");
 
-const { isAuthenticatedUser } = require("../middlewares/authMiddleware");
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/authMiddleware");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
@@ -22,6 +28,12 @@ router.put("/password/reset/:token", resetPassword );
 router.get("/logout", logout);
 
 router.get("/me", isAuthenticatedUser, getUserProfile);
+router.put("/password/update", isAuthenticatedUser, updatePassword);
+router.put("/me/update", isAuthenticatedUser, updateProfile);
 
+router.get("/admin/users", isAuthenticatedUser, authorizeRoles("admin"), allUsers);
+router.get("/admin/user/:id", isAuthenticatedUser, authorizeRoles("admin"), getUserDetails);
+router.put("/admin/user/:id", isAuthenticatedUser, authorizeRoles("admin"), updateUser);
+router.delete("/admin/user/:id", isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 module.exports = router;
