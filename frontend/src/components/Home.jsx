6e478d584f0beb68f2b48cdc0fa0778b309025e2
gsunil1996 <gajulasunilkumar123@gmail.com>
2022-06-10 21:56:51 +0530

    filter by ratings added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -30,6 +30,9 @@ const Home = ({ match }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [price, setPrice] = useState([1, 1000]);
   const [category, setCategory] = useState("");
+  const [ratings, setRatings] = useState(0);
+
+
   const categories = [
     "Electronics",
     "Cameras",
@@ -53,9 +56,9 @@ const Home = ({ match }) => {
       return alert.error(error);
      }
 
-    dispatch(getProducts(keyword, currentPage, price, category));
+    dispatch(getProducts(keyword, currentPage, price, category, ratings));
 
-  }, [dispatch, alert, error, keyword, currentPage, price, category])
+  }, [dispatch, alert, error, keyword, currentPage, price, category, ratings])
   
   let count = productsCount;
   
@@ -113,7 +116,37 @@ const Home = ({ match }) => {
                             </li>
                           ))}
                         </ul>
-                      </div>
+                        </div>
+
+                         <hr className="my-3" />
+
+                      <div className="mt-5">
+                        <h4 className="mb-3">Ratings</h4>
+
+                        <ul className="pl-0">
+                          {[5,4,3,2,1].map((star) => (
+                            <li
+                              style={{
+                                cursor: "pointer",
+                                listStyleType: "none",
+                              }}
+                              key={star}
+                              onClick={() => setRatings(star)}
+                            >
+                              <div className="rating-outer">
+                                <div className="rating-inner"
+                                  style={{
+                                  width: `${star * 20}%`
+                                }}
+                                >
+
+                                </div>
+                              </div>
+                            </li>
+                          ))}
+                        </ul>
+                        </div>
+                        
                     </div>
                   </div>
 
