e64e60d5ba271d3f0dfc967d395ce5273eadfb31
gsunil1996 <gajulasunilkumar123@gmail.com>
2022-06-10 14:41:41 +0530

    bugs fixed

--- frontend/src/redux/actions/productActions.js
+++ frontend/src/redux/actions/productActions.js
@@ -4,47 +4,34 @@ import {
   ALL_PRODUCTS_REQUEST,
   ALL_PRODUCTS_SUCCESS,
   ALL_PRODUCTS_FAIL,
+  CLEAR_ERRORS,
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
-  CLEAR_ERRORS,
 } from "../constants/productConstants.js";
 
-
-
 export const getProducts = () => async (dispatch) => {
-    try {
-    
-        dispatch({ type: ALL_PRODUCTS_REQUEST });
-      
+  try {
+    dispatch({ type: ALL_PRODUCTS_REQUEST });
 
-        const { data } = await axios.get("/api/v1/products");
-
-        dispatch({
-            type: ALL_PRODUCTS_SUCCESS,
-            payload: data
-        })
-     
-
-    } catch (error) {
-        dispatch({
-            type: ALL_PRODUCTS_FAIL,
-            payload: error.response.data.message
-          })
-    }
-}
-
+    const { data } = await axios.get("/api/v1/products");
+    // console.log(data,'data')
+    dispatch({ type: ALL_PRODUCTS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: ALL_PRODUCTS_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
 export const getProductDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST });
 
     const { data } = await axios.get(`/api/v1/product/${id}`);
-
-    dispatch({
-      type: PRODUCT_DETAILS_SUCCESS,
-      payload: data.product
-    });
+    // console.log(data,'dataInside')
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data.product });
   } catch (error) {
     dispatch({
       type: PRODUCT_DETAILS_FAIL,
@@ -53,10 +40,7 @@ export const getProductDetails = (id) => async (dispatch) => {
   }
 };
 
-// Clear Errors
-
-export const clearErrors = async (dispatch) => {
-    dispatch({
-        type: CLEAR_ERRORS
-    })
-}
+//clear Error
+export const clearErrors = () => async (dispatch) => {
+  dispatch({ type: CLEAR_ERRORS });
+};
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,17 +1,25 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { productsReducer, productDetailsReducer } from "../redux/reducers/productReducers";
+
+import {
+  productsReducer,
+  productDetailsReducer,
+} from "../redux/reducers/productReducers";
 
 const reducer = combineReducers({
   products: productsReducer,
   productDetails: productDetailsReducer,
 });
 
-let initialState = {};
+const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
