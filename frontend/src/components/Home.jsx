1e69ae308d26d47978fee06166941746f34f7ed3
gsunil1996 <gajulasunilkumar123@gmail.com>
2022-06-10 20:37:44 +0530

    filter by price added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,15 +6,23 @@ import Product from './product/Product';
 import Loader from './layouts/Loader';
 import { useAlert } from "react-alert";
 import Pagination from "react-js-pagination";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 
-const Home = ({ match, history }) => {
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range);
+
+const Home = ({ match }) => {
  
   const alert = useAlert();
   const dispatch = useDispatch();
   
   const { loading, products, error, productsCount, resultsPerPage } = useSelector(state => state.products);
+
   const [currentPage, setCurrentPage] = useState(1);
+  const [price, setPrice] = useState([1, 1000]);
+
   const keyword = match.params.keyword;
 
   useEffect(() => {
@@ -23,9 +31,9 @@ const Home = ({ match, history }) => {
       return alert.error(error);
      }
 
-    dispatch(getProducts(keyword, currentPage));
+    dispatch(getProducts(keyword, currentPage, price));
 
- }, [dispatch, alert, error, keyword, currentPage])
+ }, [dispatch, alert, error, keyword, currentPage, price])
 
   return (
     <div>
@@ -37,10 +45,45 @@ const Home = ({ match, history }) => {
           <h1 id="products_heading">Latest Products</h1>
           <section id="products" className="container mt-5">
             <div className="row">
-              {products &&
+              {keyword ? (
+                <>
+                  <div className="col-6 col-md-3 mt-5 mb-5">
+                    <div className="px-5">
+                      <Range
+                        marks={{
+                          1: `₹1`,
+                          1000: `₹1000`,
+                        }}
+                        min={1}
+                        max={1000}
+                        defaultValue={[1, 1000]}
+                        tipFormatter={(value) => `₹${value}`}
+                        tipProps={{
+                          placement: "top",
+                          visible: true,
+                        }}
+                        value={price}
+                        onChange={(price) => setPrice(price)}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-6 col-md-9">
+                    <div className="row">
+                      {products &&
+                        products.map((product) => (
+                          <Product key={product._id} product={product} col={4} />
+                        ))}
+                    </div>
+                  </div>
+                </>
+              ) : (
+                products &&
                 products.map((product) => (
-                  <Product key={product._id} product={product} />
-                ))}
+                  <Product key={product._id} product={product} col={3} />
+                ))
+                )}
+                
             </div>
           </section>
           {resultsPerPage <= productsCount && (
--- frontend/src/redux/actions/productActions.js
+++ frontend/src/redux/actions/productActions.js
@@ -10,13 +10,16 @@ import {
   PRODUCT_DETAILS_FAIL,
 } from "../constants/productConstants.js";
 
-export const getProducts = ( keyword = '', currentPage = 1) => async (dispatch) => {
+export const getProducts = ( keyword = '', currentPage = 1, price) => async (dispatch) => {
   try {
     dispatch({ type: ALL_PRODUCTS_REQUEST });
 
-    const { data } = await axios.get(`/api/v1/products?keyword=${keyword}&page=${currentPage}`);
-    // console.log(data,'data')
+    let link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[lte]=${price[1]}&price[gte]=${price[0]}`;
+
+    const { data } = await axios.get(link);
+   
     dispatch({ type: ALL_PRODUCTS_SUCCESS, payload: data });
+
   } catch (error) {
     dispatch({
       type: ALL_PRODUCTS_FAIL,
