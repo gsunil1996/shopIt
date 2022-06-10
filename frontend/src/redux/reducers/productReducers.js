c391b7220d48b28d2e993b781863feb5396b4f11
gsunil1996 <gajulasunilkumar123@gmail.com>
2022-06-10 15:09:50 +0530

    frontend added

---
+++ frontend/src/redux/reducers/productReducers.js
@@ -0,0 +1,76 @@
+import {
+  ALL_PRODUCTS_SUCCESS,
+  ALL_PRODUCTS_REQUEST,
+  ALL_PRODUCTS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  CLEAR_ERRORS,
+} from "../constants/productConstants.js";
+
+const initState = {
+  products: [],
+};
+export const productsReducer = (state = initState, action) => {
+  switch (action.type) {
+    case ALL_PRODUCTS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        products: [],
+      };
+    case ALL_PRODUCTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        products: action.payload.products,
+        productsCount: action.payload.productsCount,
+      };
+    case ALL_PRODUCTS_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
+
+const singleProduct = {
+  product: {},
+};
+export const productDetailsReducer = (state = singleProduct, action) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case PRODUCT_DETAILS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        product: action.payload,
+      };
+    case PRODUCT_DETAILS_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
